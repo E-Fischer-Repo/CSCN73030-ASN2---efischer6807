204864898dd84c13b30198396205f58d6028c5bb
E-Fischer-Repo <72356018+E-Fischer-Repo@users.noreply.github.com>
2022-09-16 10:16:16 -0400

    Add project files.

---
+++ CSCN73030 ASN2 - efischer6807/Source.cpp
@@ -0,0 +1,15 @@
+/*
+* Name: Eric Fischer
+* File: Source.cpp
+* Date: 16/09/22
+* Description: The purpose of this assignment is to walk through the development process that will be used
+during the class project. While going through the development process, you will implement a set of
+code that will contain compiler directives for controlling debug, release, and pre-release testing code
+using the Visual Studio IDE and C++.
+*/
+
+
+int main() {
+
+	return 1;
+}
