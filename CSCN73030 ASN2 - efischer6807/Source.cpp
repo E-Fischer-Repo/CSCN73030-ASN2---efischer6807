160abf5801429ea94b45e5f6451bfb477c69bb19
E-Fischer-Repo <72356018+E-Fischer-Repo@users.noreply.github.com>
2022-09-22 22:47:58 -0400

    Commit & Push as final part of Step #3.

--- CSCN73030 ASN2 - efischer6807/Source.cpp
+++ CSCN73030 ASN2 - efischer6807/Source.cpp
@@ -9,7 +9,50 @@ using the Visual Studio IDE and C++.
 */
 
 
-int main() {
+/*
+	Next you need to implement the main function to read the StudentData.txt file, create student objects and store them into a vector space. 
+	Create a struct STUDENT_DATA that contains the first and last names of the students. 
+	Each line read from the input file contains a first/last name pair separated by a comma. 
+	Parse the data, create an object and push that object into a vector space.
+	When you are finished you should have all the students listed in your vector space.
+	Commit and push your changes into the repository master.
+*/
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+	string fileName = "StudentData.txt"; // the file to be opened
+	ifstream fin;
+	class Student {
+	public:
+		struct STUDENT_DATA { // Requested student struct, holding first and last names temporarily before pushing to the vector
+			string firstName;
+			string lastName;
+		}data;
+	};
+	vector<Student> students; // The vector that will dynamically allocate space for the students
+
+	fin.open(fileName); // opens the given file
+	if (fin.is_open()) {
+		while (!fin.eof()) {
+			Student baseStudent;
+			getline(fin, baseStudent.data.lastName, ',');
+			//cout << "Last name: " << data.lastName << endl; //This code was used to test if the getline properly went past the delimiter
+			getline(fin, baseStudent.data.firstName);
+			//cout << "First name: " << data.firstName << endl; //This code was used to test if the getline properly went past the delimiter
+			students.push_back(baseStudent);
+		}
+
+	}
+	else {
+		cout << "Failed to open file!\n" << endl; // Only executes when the file was not oepned properly
+	}
 	return 1;
 }
